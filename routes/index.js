77023be9aea8fbf9f0ed652998c47768a18a01d9
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-04-24 14:28:44 -0700

    initial commit of files. added sequelize to the server.js

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.send("<h1>This is not the route you are looking for!</h1>")
+});
+
+module.exports = router;
