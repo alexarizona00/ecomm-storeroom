8d17a47535ab511a161c53ce67773f67763999d2
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-04-25 07:12:24 -0700

    fleshed out remaining routes, moving on to testing

--- models/Tag.js
+++ models/Tag.js
@@ -5,16 +5,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-   id:{
-     type: DataTypes.INTEGER,
-     allowNull: false,
-     primaryKey: true,
-     autoIncrement: true
-   },
-   tag_name: {
-     type: DataTypes.STRONG
-   }
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
+    tag_name: {
+      type: DataTypes.STRING
+    }
+  },
   {
     sequelize,
     timestamps: false,
