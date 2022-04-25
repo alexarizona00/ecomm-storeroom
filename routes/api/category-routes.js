6949627655c522ab11b2985065f256c482a421ab
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-04-24 17:38:10 -0700

    fleshed out all the models and started on the routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,21 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attribute: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+
+    });
 });
 
 router.get('/:id', (req, res) => {
